3aea832cbb2afa9a1ac310e1f828dee7cf6a26e0
WP Speed Expert <139096428+WPSpeedExpert@users.noreply.github.com>
2024-08-20 17:47:46 +0200

    Create extend-formidable-form-token.php

---
+++ extend-formidable-form-token.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Extend Formidable Form Token
+Description: Extends the times to check before today for Formidable Forms. Fix for: Form token is invalid. Please refresh the page.
+Version: 1.0
+Author: Brian Chin
+Author URI: https://wpspeedexpert.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Extend times to check before today.
+ * https://formidableforms.com/knowledgebase/frm_form_token_check_before_today/
+ */
+add_filter( 'frm_form_token_check_before_today', 'my_custom_function' );
+function my_custom_function( $times ) {
+    array_push( $times, 3 * DAY_IN_SECONDS ); // Three days ago.
+
+    return $times;
+}
